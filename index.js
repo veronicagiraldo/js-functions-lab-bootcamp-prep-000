0c54de0f0d8bb77b9ef4af0b2d25c37748dcfb1c
Veronica Giraldo <veronica9893@gmail.com>
2018-01-15 20:45:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,5 +3,10 @@ function happyHolidays () {
 return "Happy holidays!"
 }
 function happyHolidaysTo(name) {
-  return 'happy holidays, ${name}!'
+  return `happy holidays, ${name}!`
 }
+function happyHolidaysTo(name) {
+ return `Happy holidays, ${name}!`
+ }
+ 
+ 
