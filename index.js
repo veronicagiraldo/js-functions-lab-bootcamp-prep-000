5157b7089fe99d56359faf5541e635d6426582a8
Veronica Giraldo <veronica9893@gmail.com>
2018-01-15 20:52:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,3 +11,14 @@ function happyHolidaysTo(name) {
  function holidayCountdown(holiday, days) {
    return `It's ${days} days until ${holiday}!`
  }
+ function happyHolidaysTo(name) {
+   return `Happy holidays, ${name}!`
+ }
+ 
+ function happyHolidayTo(holiday, name) {
+   return `Happy ${holiday}, ${name}!`
+ }
+ 
+ function holidayCountdown(holiday, days) {
+   return `It's ${days} days until ${holiday}!`
+ }
